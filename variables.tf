94e508e62b4ca654cd470ba7ecc28aa3914a2453
Jeff Sushetski <jsushetski@gmail.com>
2021-04-25 14:44:02 -0600

    Added a variable for the combiner

--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
+variable "combiner" {
+  type        = string
+  default     = "OR"
+  description = "Alert policy condition combiner."
+
+  validation {
+    condition     = contains(["AND", "AND_WITH_MATCHING_RESOURCE", "OR",], var.combiner)
+    error_message = "The specified combiner is invalid."
+  }
+}
+
 variable "display_name" {
   type        = string
   description = "Alert policy display name."
