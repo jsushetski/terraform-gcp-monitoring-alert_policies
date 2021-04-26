ed2e23fb39a81a67852ceb68dc97fcc8699aba05
Jeff Sushetski <jsushetski@gmail.com>
2021-04-25 23:26:33 -0600

    Implemented validation for per_series_aligner

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "conditions_threshold" {
       for condition in var.conditions_threshold :
       contains(["COMPARISON_GT", "COMPARISON_GE", "COMPARISON_LT", "COMPARISON_LE", "COMPARISON_EQ", "COMPARISON_NE"], condition.comparison)
     ])
-    error_message = "The specified comparison is not valid."
+    error_message = "The value of 'comparison' is not valid."
   }
 
   validation {
@@ -49,6 +49,14 @@ variable "conditions_threshold" {
       contains(["REDUCE_NONE", "REDUCE_MEAN", "REDUCE_MIN", "REDUCE_MAX", "REDUCE_SUM", "REDUCE_STDDEV", "REDUCE_COUNT", "REDUCE_COUNT_TRUE", "REDUCE_COUNT_FALSE", "REDUCE_FRACTION_TIME", "REDUCE_PERCENTILE_99", "REDUCE_PERCENTILE_95", "REDUCE_PERCENTILE_50", "REDUCE_PERCENTILE_05"], condition.cross_series_reducer)
     ])
     error_message = "The value of 'cross_series_reducer' is not valid."
+  }
+
+  validation {
+    condition = alltrue([
+      for condition in var.conditions_threshold :
+      contains(["ALIGN_NONE", "ALIGN_DELTA", "ALIGN_RATE", "ALIGN_INTERPOLATE", "ALIGN_NEXT_OLDER", "ALIGN_MIN", "ALIGN_MAX", "ALIGN_MEAN", "ALIGN_COUNT", "ALIGN_SUM", "ALIGN_STDDEV", "ALIGN_COUNT_TRUE", "ALIGN_COUNT_FALSE", "ALIGN_FRACTION_TRUE", "ALIGN_PERCENTILE_99", "ALIGN_PERCENTILE_95", "ALIGN_PERCENTILE_50", "ALIGN_PERCENTILE_05", "ALIGN_PERCENTILE_CHANGE"], condition.per_series_aligner)
+    ])
+    error_message = "The value of 'per_series_aligner' is not valid."
   }
 }
 
