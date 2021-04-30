7d3beaa20bde6c5a515736c8a324018e41e56da4
Jeff Sushetski <jsushetski@gmail.com>
2021-04-30 16:13:09 -0600

    Fixed incorrect alert policy condition validation.

--- variables.tf
+++ variables.tf
@@ -42,7 +42,7 @@ variable "conditions_threshold" {
   validation {
     condition = alltrue([
       for condition in var.conditions_threshold :
-      condition.duration >= 1 && condition.duration <= 60
+      condition.duration % 60 == 0
     ])
     error_message = "The value of 'duration' must be between 1 and 60."
   }
